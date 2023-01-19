cf1b11f771c9bdd8bd3c94ebc1ecae16a8def5d5
Josu Martinez <josu.martinez@climatepartner.com>
2023-01-19 10:01:15 +0100

    Added ESLint rules to disable commit of code including console.log or only/skip on Jest tests.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'no-only-or-skip-tests'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'error',
+    'no-only-or-skip-tests/no-only-tests': 'error',
+    'no-only-or-skip-tests/no-skip-tests': 'error',
   },
 };
