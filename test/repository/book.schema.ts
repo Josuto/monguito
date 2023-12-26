939135aa0db58b77039a2c8ca18cfdc3f04ed783
Josu Martinez <6097850+Josuto@users.noreply.github.com>
2023-12-26 19:50:14 +0100

    feat: add type definition for extendSchema

--- test/repository/book.schema.ts
+++ test/repository/book.schema.ts
@@ -1,14 +1,19 @@
+import { Schema } from 'mongoose';
 import { BaseSchema, extendSchema } from '../../src';
+import { Book, PaperBook } from '../domain/book';
 
-export const BookSchema = extendSchema(BaseSchema, {
+export const BookSchema: Schema<Book> = extendSchema(BaseSchema, {
   title: { type: String, required: true },
   description: { type: String, required: false },
   isbn: { type: String, required: true, unique: true },
 });
 
-export const PaperBookSchema = extendSchema(BookSchema, {
-  edition: { type: Number, required: true, min: 1 },
-});
+export const PaperBookSchema: Schema<PaperBook> = extendSchema<Book, PaperBook>(
+  BookSchema,
+  {
+    edition: { type: Number, required: true, min: 1 },
+  },
+);
 
 export const AudioBookSchema = extendSchema(BookSchema, {
   hostingPlatforms: { type: [{ type: String }], required: true },
