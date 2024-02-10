d395f664bd2896db6109bd3079791909dcac0a50
Josu Martinez <6097850+Josuto@users.noreply.github.com>
2024-02-10 11:04:37 +0100

    docs: update operation options documentation
    Also performed version bump.

--- src/util/operation-options.ts
+++ src/util/operation-options.ts
@@ -3,28 +3,22 @@ import { IllegalArgumentException } from './exceptions';
 
 /**
  * Specifies options required to peform transactional operations.
+ * @property {ClientSession=} session (optional) a Mongoose session required in operations to run within a transaction.
  */
 type TransactionOptions = {
-  /**
-   * (optional) A Mongoose session required in operations to run within a transaction.
-   * @type {ClientSession}
-   */
   session?: ClientSession;
 };
 
 /**
  * Specifies options required to perform audit on side effect operation execution.
+ * @property {string=} userId (optional) the id of the user performing the operation.
  */
 type AuditOptions = {
-  /**
-   * (Optional) The id of the user performing the operation.
-   * @type {string}
-   */
   userId?: string;
 };
 
 /**
- * Page specification utility class.
+ * Specifies paging configuration for search operations.
  */
 export class Pageable {
   /**
@@ -60,25 +54,13 @@ export class Pageable {
 
 /**
  * Specifies options for the `findAll` operation.
- * @property {Pageable=} pageable
+ * @property {any=} filters (optional) filters for the search.
+ * @property {any=} sortBy (optional) sorting criteria for the search.
+ * @property {Pageable=} pageable (optional) paging configuration.
  */
 export type SearchOptions = {
-  /**
-   * (Optional) A MongoDB entity field-based query to filter results.
-   * @type {any}
-   */
   filters?: any;
-
-  /**
-   * (Optional) A MongoDB sort criteria to return results in some sorted order.
-   * @type {any}
-   */
   sortBy?: any;
-
-  /**
-   * (Optional) Page-related options.
-   * @type {Pageable}
-   */
   pageable?: Pageable;
 };
 
@@ -94,11 +76,8 @@ export type SaveAllOptions = AuditOptions;
 
 /**
  * Specifies options for the `deleteAll` operation.
+ * @property {any=} filters (optional) a MongoDB query object to select the entities to be deleted.
  */
 export type DeleteAllOptions = {
-  /**
-   * (Optional) A MongoDB query object to select the entities to be deleted.
-   * @type {any}
-   */
   filters?: any;
 };
